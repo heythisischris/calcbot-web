ae546f3ead0b8df233678434439f459784cd4c79
Chris Aitken <chris@heythisischris.com>
2023-01-15 22:37:36 -0500

    Added Tauri to produce standalone Windows binary from SvelteKit app

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import staticAdapter from "@sveltejs/adapter-static";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html',
-		})
-	}
+  kit: {
+    adapter: staticAdapter(),
+  },
 };
 
 export default config;
